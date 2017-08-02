f97b9a64ed272debd6fcfebecde0d6c23c5b6e3e
Simon CORNAZ <scornaz@e2r9p13.42.fr>
2017-08-02 17:13:48 +0200

    ok

---
+++ exos/ex07/ft_print_combn.c
@@ -0,0 +1,38 @@
+int ft_putchar(char c);
+#include "../ft_putchar.c"
+
+void print(int *table, int total)
+{
+	int i = 0;
+	while(i <= total){
+		ft_putchar(table[i]);
+		i++;
+	}
+	ft_putchar(',');
+}
+
+void f (int index, int niveau, int *table, int offset){
+
+	int i = 0;
+	int to = 10 - niveau;
+
+	table[index] += offset;
+
+	while(i <= to - offset){
+		if (index < niveau){
+			f(index + 1, niveau - 1, table, i + 1);
+		} else {
+			print(table, niveau);
+		}
+		table[index]++ ;
+		i++;
+	}
+
+	table[index] = 48;
+}
+
+void ft_print_combn (int n)
+{
+	int table[10] = {48, 48, 48, 48, 48, 48, 48, 48, 48, 48};
+	f(0, n, table, 0);
+}
