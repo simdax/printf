bc918aab10e849f423b31fe3e2f9ea485039cef1
Simon CORNAZ <scornaz@e2r10p16.42.fr>
2017-08-03 14:29:19 +0200

    last

--- exos/ex07/ft_print_combn.c
+++ exos/ex07/ft_print_combn.c
@@ -6,36 +6,40 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 09:21:42 by scornaz           #+#    #+#             */
-/*   Updated: 2017/08/03 13:59:15 by scornaz          ###   ########.fr       */
+/*   Updated: 2017/08/03 14:28:47 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_putchar(char c);
 
-#include <stdio.h>
-
-void 	print(int *table, int total)
+void	print(int *table, int total)
 {
 	int i;
 
 	i = 0;
-	while (i <= total){
+	while (i <= total)
+	{
 		ft_putchar(table[i]);
 		i++;
 	}
-	ft_putchar(',');
+	if (table[0] != 57 - total)
+	{
+		ft_putchar(',');
+	}
 }
 
-void	f(int index, int niveau, int *table, int offset){
+void	f(int index, int niveau, int *table, int offset)
+{
 	int i;
 	int to;
 
 	i = 0;
 	to = 10 - niveau;
 	table[index] += offset;
-
-	while (i <= to - offset){
-		if (niveau > 1){
+	while (i <= to - offset)
+	{
+		if (niveau > 1)
+		{
 			f(index + 1, niveau - 1, table, i + (1 + offset));
 		}
 		else
@@ -45,12 +49,19 @@ void	f(int index, int niveau, int *table, int offset){
 		table[index]++;
 		i++;
 	}
-
 	table[index] = 48;
 }
 
-void	ft_print_combn (int n)
+void	ft_print_combn(int n)
 {
-	int table[10] = {48, 48, 48, 48, 48, 48, 48, 48, 48, 48};	
+	int table[10];
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		table[i] = 48;
+		i++;
+	}
 	f(0, n, table, 0);
 }
