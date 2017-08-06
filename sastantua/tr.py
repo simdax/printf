07953f66e2c81ebe2dfab54a71f120d56c2431d8
simdax <simoncornaz@gmail.com>
2017-08-06 12:49:22 +0200

    ok

--- sastantua/tr.py
+++ sastantua/tr.py
@@ -14,22 +14,7 @@ def triangle_modulo(level):
         return 0
     else:
         return (1 + (level/2)) * 2 + triangle_modulo(level - 1)
-
-def espacesPlus(niveau, niveau_global):
-    if niveau_global < 4:        
-        return 0
-    else:
-        if niveau < 0:
-            return 1
-        else:
-            return niveau%2 + niveau/2 + espacesPlus(niveau - 2, niveau_global)
     
-def calculate_spaces(level, lignes):
-    if(level > 0):
-        return lignes + calculate_spaces(level-1,lignes+1)
-    else:
-        return 0
-
 def draw(times, char):
     if(times > 0):
        sys.stdout.write(char)
@@ -50,11 +35,11 @@ def draw_door(stars, niveau, ligne):
     if niveau % 2 == 0:
         draw(niveau/2, '*')
 
-def w(lvl):
+def nbLines(lvl):
     if lvl <= 0:
         return 0
     else:
-        return lvl+2 + w(lvl-1)
+        return lvl+2 + nbLines(lvl-1)
     
 def where(width, number):
     return (width - number) / 2
@@ -64,26 +49,18 @@ def calcStars(globLine, i):
     amountPlus= triangle_modulo(i - 4)
     return triangle(globLine) + amount + amountPlus
 
-niveau = 5
+niveau = 4
 i = 1
-j = -2
 globLine = 0
-width = calcStars(w(niveau), niveau) + 2
-print width
+width = calcStars(nbLines(niveau), niveau) + 2
 while i <= niveau:
-    j += 1
     reverse = niveau - i
     lignes = i + 2
-    espaces = calculate_spaces(reverse,lignes) + 3 * reverse 
-    plus = espacesPlus(i - 1, niveau)
-    espaces +=  plus
     for x in range(0,lignes):
         globLine += 1
         stars = calcStars(globLine, i)
-        esp = where(width, stars) - 1
-        print where(width, stars) - 1
-        draw(esp, ' ')
-#        draw(lignes - x, ' ')
+        espaces = where(width, stars) - 1
+        draw(espaces, ' ')
         draw(1, '/')
         if i == niveau and x > 1:
             draw_door(stars, niveau, x)
