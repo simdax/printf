083854403c5667ba06f77d52dded349d52a833af
Simon CORNAZ <scornaz@e1r2p8.42.fr>
2017-08-07 10:57:28 +0200

    begin queens

--- j04/ex08/ft_eight_queens_puzzle.c
+++ j04/ex08/ft_eight_queens_puzzle.c
@@ -1,10 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_eight_queens_puzzle.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/07 10:55:14 by scornaz           #+#    #+#             */
+/*   Updated: 2017/08/07 10:57:25 by scornaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int		main(int size)
+void	put_queen(int tab[8][8], int x, int y)
 {
+	tab[x][y] = 1;
+	p_mat(tab);
+}
+
+void	p_mat(int tab[8][8])
+{
+	int i;
+	int j;
+
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			printf("%d", tab[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(void)
+{
+	int size;
 	int i;
 	int j;
 	int tab[8][8];
 
+	size = 8;
 	i = 0;
 	while (i < size)
 	{
@@ -16,24 +52,7 @@ int		main(int size)
 		}
 		i++;
 	}
-	
-	put_queen()
-
+	put_queen(tab, 1, 2);
 	return (0);
 }
 
-void	p_mat(int tab[8][8]){
-	int i;
-	int j;
-	while (i < size)
-	{
-		j = 0;
-		while (j < size)
-		{
-			printf("%d", tab[i][j])
-			j++;
-		}
-		i++;
-	}
-	
-}
