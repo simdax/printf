672b9b9f7a1e8281f2d4f1b9cf184fc4f286b31a
Simon CORNAZ <scornaz@e1r2p8.42.fr>
2017-08-07 15:13:40 +0200

    finish 8 q

--- j04/ex08/ft_eight_queens_puzzle.c
+++ j04/ex08/ft_eight_queens_puzzle.c
@@ -6,31 +6,9 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/07 10:55:14 by scornaz           #+#    #+#             */
-/*   Updated: 2017/08/07 14:57:52 by scornaz          ###   ########.fr       */
+/*   Updated: 2017/08/07 15:13:09 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-
-void	p_mat(int tab[8][8], int size)
-{
-	int i;
-	int j;
-
-	i = 0;
-	while (i < size)
-	{
-		j = 0;
-		while (j < size)
-		{
-			printf("%d ", tab[i][j]);
-			j++;
-		}
-		i++;
-		printf("\n");
-	}
-	printf("\n");
-}
 
 void	update_mat(int tab[8][8], int x, int y, int increment)
 {
@@ -55,51 +33,52 @@ void	update_mat(int tab[8][8], int x, int y, int increment)
 	i = 0;
 	while (x + ++i < 8 && y - i >= 0)
 		tab[x + i][y - i] += increment;
+	p_mat(tab, 8);
 }
 
-int		put_queen(int tab[8][8], int col)
+int		put_queen(int tab[8][8], int col, int *solutions)
 {
 	int i;
 	int solution;
 
 	while (i < 8)
 	{
-		if(tab[i][col] == 0)
+		if (tab[i][col] == 0)
 		{
-			update_mat(tab, i, col, 1);
-			solution = put_queen(tab, col + i);
-			if (!solution) 
-				update_mat(tab, i, col, -1);
+			if (col == 7)
+			{
+				(*solutions)++;
+			}
+			else
+			{
+				update_mat(tab, i, col, 1);
+				if (!put_queen(tab, col + 1, solutions))
+					update_mat(tab, i, col, -1);
+			}
 		}
 		i++;
 	}
 	return (0);
 }
 
-int		main(void)
+int		ft_eight_queens_puzzle(void)
 {
-	int size;
+	int solutions;
 	int i;
 	int j;
 	int tab[8][8];
 
-	size = 8;
+	solutions = 0;
 	i = 0;
-	while (i < size)
+	while (i < 8)
 	{
 		j = 0;
-		while (j < size)
+		while (j < 8)
 		{
 			tab[i][j] = 0;
 			j++;
 		}
 		i++;
 	}
-
-	update_mat(tab, 7, 7, 1);
-	p_mat(tab, 8);
-	update_mat(tab, 0, 0, 1);
-	p_mat(tab, 8);
-
-	return (0);
+	return (solutions);
 }
