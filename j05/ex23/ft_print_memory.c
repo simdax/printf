bb700a3a51b983efaced9dcab4effdf4b6dc7f34
Simon CORNAZ <scornaz@e1r2p10.42.fr>
2017-08-08 14:29:33 +0200

    first end d5

--- j05/ex23/ft_print_memory.c
+++ j05/ex23/ft_print_memory.c
@@ -6,11 +6,72 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/08 12:01:03 by scornaz           #+#    #+#             */
-/*   Updated: 2017/08/08 12:14:22 by scornaz          ###   ########.fr       */
+/*   Updated: 2017/08/08 14:29:12 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_putchar(char c);
+
+int		power_of(int nb, int power)
+{
+	if (power == 0)
+		return (1);
+	return (nb * power_of(nb, power - 1));
+}
+
+void	ft_putnbr_base(int nb, char *base, int result_length)
+{
+	int		length_base;
+	char	*result;
+	int		pow;
+
+	length_base = 0;
+	while (base[length_base])
+		length_base++;
+	if (length_base > 1)
+	{
+		while (result_length > 0)
+		{
+			pow = power_of(length_base, result_length - 1);
+			ft_putchar(base[nb / pow]);
+			nb = nb % pow;
+			result_length--;
+		}
+	}
+}
+
+void	print_non_printable(char c)
+{
+	if (0 >= c || c < 32)
+		ft_putchar('.');
+	else
+		ft_putchar(c);
+}
+
+void	print(int *counter, int len, void *addr)
+{
+	int plus_seize;
+
+	plus_seize = (*counter + len);
+	while (*counter < plus_seize)
+	{
+		ft_putnbr_base(*(char *)addr, "0123456789abcdef", 2);
+		if (*counter % 2 == 1)
+			ft_putchar(' ');
+		addr++;
+		(*counter)++;
+	}
+	addr -= len;
+	(*counter) -= len;
+	ft_putchar(' ');
+	plus_seize = (*counter + len);
+	while (*counter < plus_seize)
+	{
+		print_non_printable(*(char *)addr);
+		addr++;
+		(*counter)++;
+	}
+}
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
@@ -19,15 +80,12 @@ void	*ft_print_memory(void *addr, unsigned int size)
 	i = 0;
 	while (i < size)
 	{
-		ft_putchar(*addr);
+		ft_putnbr_base(i, "0123456789abcdef", 8);
+		ft_putchar(':');
+		ft_putchar(' ');
+		print(&i, 16, addr);
 		ft_putchar('\n');
-		addr++;
-		i++;
+		addr += 16;
 	}
 	return (addr);
-}
-
-void	main(void)
-{
-	ft_print_memory("coucou", 16);
 }
