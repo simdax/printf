39ada33b5b76db97be80cf054b64904906c25e4c
Simon CORNAZ <scornaz@e1r2p10.42.fr>
2017-08-09 12:57:39 +0200

    atoi

--- j05/ex02/ft_atoi.c
+++ j05/ex02/ft_atoi.c
@@ -6,44 +6,65 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 23:28:15 by scornaz           #+#    #+#             */
-/*   Updated: 2017/08/05 19:02:33 by scornaz          ###   ########.fr       */
+/*   Updated: 2017/08/09 12:57:16 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		check_neg(char sign, int *length)
+int		check_neg(char *str, int *length)
 {
+	char sign;
+
+	sign = str[*length];
 	if (sign == '-')
 	{
-		*length = 1;
+		*length += 1;
 		return (1);
 	}
-	else
+	else if (sign == '+')
 	{
-		*length = 0;
+		*length += 1;
 		return (0);
 	}
+	else if (sign == ' ')
+	{
+		*length += 1;
+		return (check_neg(str, length));
+	}
+	else if (47 > sign || sign < 57)
+		return (0);
+	else
+		return (-1);
+}
+
+void	calc(int *res, char *str, int length)
+{
+	int decimal;
+	int i;
+
+	decimal = 1;
+	i = length + 1;
+	while (str[i])
+	{
+		decimal *= 10;
+		i++;
+	}
+	*res += (str[length] - 48) * decimal;
 }
 
 int		ft_atoi(char *str)
 {
 	int		res;
-	int		decimal;
 	int		length;
-	int		i;
 	int		is_neg;
 
-	is_neg = check_neg(*str, &length);
+	length = 0;
+	is_neg = check_neg(str, &length);
+	if (is_neg == -1)
+		return (0);
 	res = 0;
 	while (str[length])
 	{
-		decimal = 1;
-		i = length + 1;
-		while (str[i])
-		{
-			decimal *= 10;
-			i++;
-		}
-		res += (str[length] - 48) * decimal;
+		calc(&res, str, length);
 		length++;
 	}
 	if (is_neg)
--- j05/main/ex02/main.c
+++ j05/main/ex02/main.c
@@ -1,12 +1,34 @@
 int ft_atoi(char*);
 
 #include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	prtinf("%d", ft_atoi(42));
-	prtinf("%d", ft_atoi(42));
-	prtinf("%d", ft_atoi(42));
-	prtinf("%d", ft_atoi(42));
+
+//	printf("%d\n", INT_MAX - INT_MAX < 0);
+
+	printf("%d\n", ft_atoi("+0422"));
+	printf("%d\n", ft_atoi("     0422"));
+	printf("%d\n", ft_atoi(" 4 2"));
+	printf("%d\n", ft_atoi("-42"));
+	printf("%d\n", ft_atoi(" -2147483648"));
+	printf("%d\n", ft_atoi("+2147483647"));
+	printf("%d\n", ft_atoi("   fdsnfja"));
+	printf("%d\n", ft_atoi("  45612345645135456456421"));
+
+	printf("\n ////////// \n\n");
+
+	printf("%d\n", atoi("+0422"));
+	printf("%d\n", atoi("    0422"));
+	printf("%d\n", atoi(" 4  2"));
+	printf("%d\n", atoi("-42"));
+	printf("%d\n", atoi(" -2147483648"));
+	printf("%d\n", atoi("+2147483647"));
+	printf("%d\n", atoi("   fdsnfja"));
+	printf("%d\n", atoi("       45612345645135456456421"));
+
 	return (0);
 }
+
