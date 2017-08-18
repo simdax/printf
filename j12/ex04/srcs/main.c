a7f3457f258e9dd3bd95977db6048eaaf7e16c00
Simon CORNAZ <scornaz@e1r2p14.42.fr>
2017-08-18 10:08:18 +0200

    io

--- j12/ex04/srcs/main.c
+++ j12/ex04/srcs/main.c
@@ -1,28 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header.h                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/16 15:30:46 by scornaz           #+#    #+#             */
-/*   Updated: 2017/08/18 09:44:15 by scornaz          ###   ########.fr       */
+/*   Created: 2017/08/18 09:17:40 by scornaz           #+#    #+#             */
+/*   Updated: 2017/08/18 09:46:05 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __HEADER_H
-#define __HEADER_H
+#include "header.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
+int main(int argc, char **argv)
+{
+	int				fd;
+	unsigned int	ret;
+	int 			size;
+	char			buf[BUF_SIZE + 1];
 
-#define ARGS_ERROR		write(2, "Too many arguments.", 30)
-#define FILENAME_ERROR	write(2, "File name missing.", 30)
-#define BUF_SIZE		64
-
-void	*ft_print_memory(void *str, unsigned int len);
-void	ft_putchar(char c);
-
-#endif
+	write(1, "ta mere", 10);
+	if (argc == 1)
+		FILENAME_ERROR;
+	else
+	{
+		while (*argv)
+		{
+			fd = open(*argv, O_RDONLY);
+			while ((ret = read(fd, buf, BUF_SIZE)))
+				   ft_print_memory(buf, ret);
+			ft_putchar('\n');
+		}
+		close(fd);
+	}
+}
