08e9d1ae6bf1e2b69f2771e412959cc36db0df2f
simdax <simoncornaz@gmail.com>
2017-12-04 22:17:07 +0100

    c_conppile

--- 42/printf/parser/srcs/main.c
+++ 42/printf/parser/srcs/main.c
@@ -2,11 +2,10 @@
 
 int main(int argc, char **argv)
 {
-  printf("%0456.0d", 454564014484550);
   (void)argv;
   if (argc == 2)
     {
-     
+      
     }
   
 }
--- 42/printf/parser/srcs/parser.c
+++ 42/printf/parser/srcs/parser.c
@@ -1,26 +1,38 @@
-static int	is_flag()
-{
-  /* int i; */
+#include "parser.h"
 
-  /* i = 0; */
-  /* while (*str) */
-  /*   { */
-  /*     if (*str == '#' || *str == '0' || */
-  /* 	  *str == ' ' || *str == '+' || */
-  /* 	  *str == '-') */
-  /* 	break; */
-  /*     i++ */
-  /*   } */
-  /* return (i); */
+static char	*is_flag(char *str)
+{
+  int i;
+
+  i = 0;
+  while (*str)
+    {
+      if (*str == '#' || *str == '0' ||
+  	  *str == ' ' || *str == '+' ||
+  	  *str == '-')
+	{i++;}
+      else
+	break ;
+      str++;
+    }
+  return (ft_strndup(str, i));
 }
 
-#include "parser.h"
+static 
 
 char	**parser(char *str)
 {
-  /* char	*res[4]; */
+  char	*res[4];
   
-  /* res[0] = ft_strsub */
-
-  /* return (res); */
+  res[0] = is_flag(str);
+  str += ft_strlen(res[0]);
+  res[1] = ft_atoi(str);
+  str += ft_strlen(res[1]);
+    if (str[0] == '.')
+      {
+	res[2] = ft_atoi(str);
+	str += ft_strlen(res[1]);
+      }
+  res[3] = str[0];
+  return (res);
 }
