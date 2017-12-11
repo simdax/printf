0f72708d8a54f52111c1febdf7dc773dd90a42fe
simdax <simoncornaz@gmail.com>
2017-12-11 21:01:00 +0100

    tests OK

--- 42/printf/parser/srcs/parser.c
+++ 42/printf/parser/srcs/parser.c
@@ -14,35 +14,38 @@
 
 static void	take_flags(char **s, t_flags *flags)
 {
-	char		*str = *s;
+    char		*str = *s;
 
-	while (*str)
+    while (*str)
 	{
-		SET(flags->zero, '0', *str);
-		SET(flags->plus, '+', *str);
-		SET(flags->hash, '#', *str);
-		SET(flags->minus, '-', *str);
-		SET(flags->space, ' ', *str);
-		SET(flags->apostrophe, 39, *str);
-		if (!ft_strany(*str, "0#+- '"))
-			break;
-		*s = ++str;
+	    SET(flags->zero, '0', *str);
+	    SET(flags->plus, '+', *str);
+	    SET(flags->hash, '#', *str);
+	    SET(flags->minus, '-', *str);
+	    SET(flags->space, ' ', *str);
+	    SET(flags->apostrophe, 39, *str);
+	    if (!ft_strany(*str, "0#+- '"))
+		break;
+	    *s = ++str;
 	}
 }
 
 t_flags				parse(char *str)
 {
-  	t_flags		flags;
-	char		*point;
+    t_flags		flags;
 
-	flags = (t_flags){0, 0, 0, 0, 0, 0, 0, 0, 0};
-	take_flags(&str, &flags);
-	point = ft_strchr(str, '.');
-	if (point && point[1])
-	  {
-	    flags.width = ft_atoi(str);
-	    flags.precision = ft_atoi(point + 1);
-	    flags.type = point[ft_nbrsize(flags.precision) + 1];
-	  }
-	return (flags);
+    flags = (t_flags){0, 0, 0, 0, 0, 0, 0, 0, 0};
+    take_flags(&str, &flags);
+    flags.width = ft_atoi(str);
+    str += flags.width ? ft_nbrsize(flags.width) : 0;
+    if (*str == '.')
+	{
+	    ++str;
+	    flags.precision = ft_atoi(str);
+	    str += flags.precision ?
+		ft_nbrsize(flags.precision) : 0;
+	}
+    if (*str)
+	flags.type = str[0];
+    return (flags);
 }
